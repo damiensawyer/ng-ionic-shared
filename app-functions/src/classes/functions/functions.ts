1057acacd1aac04f7b87cd8b0f737b069489a217
Damien Sawyer <damiensawyer@gmail.com>
2017-11-01 07:01:40 +0100

    test

--- app-functions/src/classes/functions/functions.ts
+++ app-functions/src/classes/functions/functions.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FunctionsService {
+  message = 'This was generated from shared service';
+
   constructor() {}
 
   addNumbers(first: number, second: number) {
     return first + second;
   }
+
 }
--- app-functions/src/component/sample/sample.component.spec.ts
+++ app-functions/src/component/sample/sample.component.spec.ts
@@ -1,18 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
 
-import { SampleComponent } from './sample.component';
-import { FunctionsService } from 'classes/functions/functions';
+import { SampleComponent } from "./sample.component";
+import { FunctionsService } from "classes/functions/functions";
 // importing from jest.... https://stackoverflow.com/a/42035003
 
-describe('Another set of tests', () => {
-  it('should work...', () => {
+describe("Another set of tests", () => {
+  it("should work...", () => {
     expect(123).toBe(123);
   });
 });
 
-describe('SampleComponent', () => {
+describe("SampleComponent", () => {
   let comp: SampleComponent;
   let fixture: ComponentFixture<SampleComponent>;
   let de: DebugElement;
@@ -29,16 +29,20 @@ describe('SampleComponent', () => {
     comp = fixture.componentInstance; // BannerComponent test instance
 
     // query for the title <h1> by CSS element selector
-    de = fixture.debugElement.query(By.css('h1'));
+    de = fixture.debugElement.query(By.css("h1"));
     el = de.nativeElement;
   });
 
-  it('Should be false', () => {
+  it("Should be false", () => {
     expect(false).toBe(false);
   });
 
-  it('should use the service', () => {
+  it("should use the service", () => {
     const result = comp.useAddService(10, 20);
     expect(result).toBe(30);
+  });
+
+  it("should have message", () => {
+    expect(comp.functions.message).toBeTruthy();
   });
 });
